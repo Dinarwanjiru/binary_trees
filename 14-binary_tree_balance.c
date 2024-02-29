e105aed8d4e0cf90120cc88a0abf0365daa8c092
Phinehas <walburphinehas78@gmail.com>
2024-02-29 13:28:16 +0300

    Betty style of coding validation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -34,8 +34,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = tree->left ? 1 + binary_tree_height(tree->left): 0;
-	right_height = tree->right ? 1 + binary_tree_height(tree->right): 0;
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
 	return (left_height - right_height);
 }
